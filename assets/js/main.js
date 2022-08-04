b0ff2e4ebef04d7c41f7462384ca11ee62a12baa
supercode <getlucked@gmx.de>
2022-08-04 09:07:45 +0200

    end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 let outputVorne = document.getElementById("outputVorne")
 let outputHinten = document.getElementById("outputHinten")
 
@@ -7,29 +6,34 @@ const trenneAlles = (event) => {
     event.preventDefault()
     let txtInput = document.forms[0].txtInput.value
     let signInput = document.forms[0].signInput.value
-    let index = txtInput.indexOf(signInput);
-    let endIndex = index + signInput.length - 1;
+    let index = txtInput.search(signInput);
     let trennen = document.forms[0].trennen.value
 
 
-    if (index !== -1) {
-        outputVorne.innerHTML = "Dieses Wort ist nicht auffindbar Babe"
+    const vorTrennen = () => {
+        let textdavor1 = txtInput.slice(0, index)
+        outputVorne.innerHTML = textdavor1
+        let textdanach1 = txtInput.slice(index)
+        outputHinten.innerHTML = textdanach1
+
+
     }
-    else if
-        (trennen === "davor") {
+    const danachTrennen = () => {
+        let textdavor2 = txtInput.slice(0, index + signInput.length)
+        outputVorne.innerHTML = textdavor2
+        let textdanach2 = txtInput.slice(index + signInput.length)
+        outputHinten.innerHTML = textdanach2
+
+
+    }
+    if (index === -1) {
+        outputVorne.innerHTML = "Dieses Wort ist nicht auffindbar Babe"
+
+    } else if (trennen === "davor") {
         vorTrennen()
+
     }
     else {
         danachTrennen()
-    }
-
-
-    let vorTrennen = () => {
-        let textausgabe1 = txtInput.slice(0, index)
-        outputVorne.innerHTML = textausgabe1
-    }
-    let danachTrennen = () => {
-        let textausgabe2 = txtInput.slice(endIndex, -1)
-        outputHinten.innerHTML = textausgabe2
     }
 }
